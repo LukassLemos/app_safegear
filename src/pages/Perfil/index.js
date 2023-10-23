78e844429b2f0ab3eaf596bee17c57e28a21bc95
LukassLemos <124939307+LukassLemos@users.noreply.github.com>
2023-10-23 18:11:05 -0300

    atualização
    atualização

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Modal } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Modal, Image, Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library';
 import firebase from '../../services/firebaseConnection';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
@@ -10,6 +12,7 @@ export default function Perfil() {
   const [email, setEmail] = useState('');
   const [isEditingName, setIsEditingName] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
+  const [avatarSource, setAvatarSource] = useState(null);
 
   useEffect(() => {
     const user = firebase.auth().currentUser;
@@ -56,7 +59,7 @@ export default function Perfil() {
 
     if (user) {
       user.delete().then(() => {
-        console.log('Conta Excluída')
+        console.log('Conta Excluída');
         navigation.navigate('Welcome');
       }).catch(error => {
         console.error(error);
@@ -64,11 +67,91 @@ export default function Perfil() {
     }
   };
 
+  const handleTakePhoto = async () => {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permissão para acessar a câmera negada');
+      return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      const selectedImage = result.assets[0];
+      setAvatarSource(selectedImage.uri);
+
+      // Salvar a imagem na galeria
+      try {
+        const asset = await MediaLibrary.createAssetAsync(selectedImage.uri);
+        console.log('Imagem salva na galeria com ID:', asset.id);
+      } catch (error) {
+        console.error('Erro ao salvar a imagem na galeria:', error);
+      }
+    }
+  };
+
+  // Função para exibir um alerta com as opções
+  const showImagePickerOptions = () => {
+    Alert.alert(
+      'Escolha uma opção',
+      'Deseja tirar uma nova foto ou escolher da galeria?',
+      [
+        {
+          text: 'Tirar foto',
+          onPress: handleTakePhoto,
+        },
+        {
+          text: 'Escolher da galeria',
+          onPress: handleSelectProfileImage,
+        },
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const handleSelectProfileImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permissão para acessar a galeria negada');
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      const selectedImage = result.assets[0];
+      setAvatarSource(selectedImage.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      
+      {avatarSource ? (
+        <TouchableOpacity onPress={showImagePickerOptions}>
+          <Image source={{ uri: avatarSource }} style={styles.avatar} />
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity onPress={showImagePickerOptions}>
+          <View style={styles.cameraIconContainer}>
+            <Icon name="camera" size={30} color="#238dd1" style={styles.cameraIcon} />
+          </View>
+        </TouchableOpacity>
+      )}
       <View>
-      <View style={{ borderBottomWidth: 1, borderBottomColor: 'black', marginBottom: 30, marginTop: 130 }}></View>
+        <View style={{ borderBottomWidth: 1, borderBottomColor: 'black', marginBottom: 30, marginTop: 20 }}></View>
         <Text style={styles.label}>Nome</Text>
         {isEditingName ? (
           <TextInput
@@ -81,18 +164,19 @@ export default function Perfil() {
         )}
       </View>
       <View>
-      <Text style={styles.label}>Email</Text>
+        <Text style={styles.label}>Email</Text>
         {isEditingName ? (
-            <TextInput
-              style={styles.input}
-              value={email}
-              onChangeText={setEmail}
-              editable={false}
-            />
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            editable={false}
+          />
         ) : (
-            <Text style={styles.text}>{email}</Text>
+          <Text style={styles.text}>{email}</Text>
         )}
       </View>
+      
       {isEditingName && (
         <TouchableOpacity onPress={handleUpdateProfile} style={styles.button}>
           <Text style={styles.buttonText}>Salvar</Text>
@@ -124,7 +208,7 @@ export default function Perfil() {
                 <TouchableOpacity style={[styles.modalButton, styles.modalButtonSim]} onPress={() => {
                   handleDeleteAccount();
                   setModalVisible(false);
-                }}>
+                  }}>
                   <Text style={styles.modalButtonText}>Sim</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={[styles.modalButton, styles.modalButtonCancelar]} onPress={() => setModalVisible(false)}>
@@ -155,6 +239,7 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 18,
     marginBottom: 4,
+    
   },
 
   input: {
@@ -182,7 +267,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
-  
+
   modalContainer: {
     flex: 1,
     justifyContent: 'center',
@@ -225,21 +310,70 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
 
-   modalButtonsContainer: {
+  modalButtonsContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
 
-  botaoexcluir:{
-    fontSize:18,
-    marginTop:100,
+  botaoexcluir: {
+    fontSize: 18,
+    marginTop: 30,
   },
 
   icone: {
-    marginRight: 10, // Adicione margem direita ao ícone para separá-lo do text
-    marginTop:101,
-    marginLeft: 70
+    marginRight: 10,
+    marginTop: 32,
+    marginLeft: 70,
   },
-  
+
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 75,
+    marginBottom: 20,
+    marginLeft: 100,
+    marginTop: 15
+  },
+
+  cameraIconContainer: {
+    position: 'relative',
+  },
+
+  cameraIcon: {
+    marginTop: 20,
+    marginLeft: 150
+  },
+
+  cameraIconCircle: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: 130,
+    height: 130,
+    borderRadius: 75,
+    borderWidth: 2,
+    borderColor: 'black',
+    marginLeft: 100
+  },
 });
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+ 
+
+
+
 
